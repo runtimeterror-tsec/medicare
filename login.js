3298ff1676f438cd2e81e394572c694ebcecf9a8
runtimeterror-tsec <67727767+runtimeterror-tsec@users.noreply.github.com>
2020-07-02 17:55:36 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -14,8 +14,44 @@ function login(){
     });
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          window.location.href = "dashboard.html"
-        }
+          //window.location.href = "dashboard.html"
+          database = firebase.database();
+          var Email = document.getElementById("email_field").value;
+          var inputtypefield =$('input[name=usertype]:checked').val();
+            //console.log();
+            return database.ref('Users').orderByChild('Email').equalTo(Email).once('value').then(function(snapshot) {
+              
+              snapshot.forEach(function(userSnapshot) {
+                var usertype = userSnapshot.val().Usertype;
+                console.log(usertype);
+                if(usertype=="doctor"){
+                  window.location.href = "doctordashboard.html";
+                }
+                else if (usertype == "patient")
+                {
+                  window.location.href = "patientdashboard.html";
+                }
+                else if (usertype == "admin")
+                {
+                  window.location.href = "admindashboard.html";
+                }
+                else{
+                  //firebase.auth().currentUser.delete().then(function() {// User deleted.});
+                  console.log("No usertype defined");
+                }
+
+              })
+
+            });
+
+
+
+      } else {
+        // No user is signed in.
+        console.log("Else");
+      }
+
+        //}
       });
   }
 
